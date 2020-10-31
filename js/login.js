0fe8b56c8610a2ce1cf106fb2a2922a08666fa1f
AbilaMay <mayara_abila@hotmail.com>
2020-10-30 22:13:22 -0300

    Adicionando JavaScript para a página login.html

--- js/login.js
+++ js/login.js
@@ -1,1 +1,16 @@
-function
+function validate(e){
+   
+    //impedir recarregamento da página
+    e.preventDefault();
+
+    //.value para buscar o valor do input
+    var email = document.getElementById("user").value;
+    var pass = document.getElementById("password").value;
+
+    // validação de dados em desenvolvimento
+    if (email == 'academy@cocreare.com.br' && pass == '123456'){
+        window.location.href = "./search_area.html"
+    } else {
+        alert ('Dados incorretos. Por favor, tente novamente!')
+    }
+}
